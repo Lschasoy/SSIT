a0a573867fdaf2e87ae2248142fe2fe15f590849
lschasoy <alu3270@etsii.ull.es>
2014-03-16 18:39:41 +0000

    Commit - Nueva Interfaz

--- TFC_PROY_BETA/src/Interfaz.java
+++ TFC_PROY_BETA/src/Interfaz.java
@@ -13,17 +13,21 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.FlowLayout;
+import java.awt.Component;
 
 
 public class Interfaz {
 
 	private JFrame miVentana;
-	private JPanel Npanel, ImagActPanel, ImagFinalPanel, centralPanel;
+	private JPanel Npanel, ImagActPanel, ImagFinalPanel, centralPanel, panelSup;
 	private JButton seleccionar, girarIZQ, girarDCH, ZoomPlus, ZoomMinus, recortar;
 	private JButton cargar, salvar, salir;
 	public  BufferedImage originalImage, finalImage;
 	
-	private float xScaleFactor = 1, yScaleFactor = 1;
+	private float xScaleFactor = 1, yScaleFactor = 1, degree;
 
 	/**
 	 * Launch the application.
@@ -57,34 +61,46 @@ public class Interfaz {
 		miVentana.setSize(1024, 480);
 		
 		ImagActPanel = new JPanel();			
-		ImagActPanel.setBackground(Color.WHITE);
+		ImagActPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.DARK_GRAY, null, null, null));
+		ImagActPanel.setBackground(Color.LIGHT_GRAY);
 		ImagActPanel.setAutoscrolls(true);
 		
 		centralPanel = new JPanel();
 		centralPanel.setLayout(new GridLayout(1,2));
 
 		ImagFinalPanel =  new JPanel();
-		ImagFinalPanel.setBackground(Color.BLACK);
+		ImagFinalPanel.setBorder(new CompoundBorder(new BevelBorder(BevelBorder.LOWERED, new Color(128, 128, 128), null, null, null), null));
+		ImagFinalPanel.setBackground(Color.LIGHT_GRAY);
 		ImagFinalPanel.setAutoscrolls(true);
 		
 		centralPanel.add(ImagActPanel);					
 	    centralPanel.add(ImagFinalPanel); 
+	    
+	    panelSup = new JPanel();
+		panelSup.setLayout(new BoxLayout(panelSup, BoxLayout.Y_AXIS));
+		panelSup.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+		panelSup.setBackground(Color.white);
+		panelSup.add(panelMenu());
+		panelSup.add(panelTool());
 		
-		miVentana.getContentPane().add(Panel_SUP(), BorderLayout.NORTH);
-		miVentana.getContentPane().add(Panel_IZQ(), BorderLayout.SOUTH);
+	    
+		
+		miVentana.getContentPane().add(panelSup, BorderLayout.NORTH);		
 		miVentana.getContentPane().add(centralPanel,BorderLayout.CENTER);
 				
 	}
 	
 	/*************************************************/
-	public JPanel Panel_SUP(){
+	public JPanel panelMenu(){
 
-		Npanel = new JPanel();
-		Npanel.setLayout(new BoxLayout(Npanel, BoxLayout.X_AXIS));
-		Npanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 5, 5));
-		Npanel.setBackground(Color.LIGHT_GRAY);
+		JPanel Mpanel = new JPanel();
+		Mpanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		Mpanel.setLayout(new BoxLayout(Mpanel, BoxLayout.X_AXIS));
+		Mpanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 5, 5));
+		Mpanel.setBackground(Color.LIGHT_GRAY);
 
 		cargar = new JButton("LOAD");
+		cargar.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		cargar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				 ImagActPanel.removeAll();
@@ -102,25 +118,25 @@ public class Interfaz {
 		
 		salvar.setMaximumSize(salvar.getMaximumSize());
 
-		Npanel.add(cargar);	
-		Npanel.add(salvar);	
+		Mpanel.add(cargar);	
+		Mpanel.add(salvar);	
 
-		return Npanel;
+		return Mpanel;
 	}
 	/*************************************************/
-	public JPanel Panel_IZQ(){
+	public JPanel panelTool(){
 
 		Npanel = new JPanel();
+		Npanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		Npanel.setLayout(new BoxLayout(Npanel, BoxLayout.X_AXIS));
 		Npanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 25, 25));
-
 		Npanel.setBackground(Color.LIGHT_GRAY);
 
-		seleccionar = new JButton("Seleccionar");
-		
+		seleccionar = new JButton("Seleccionar");		
 		seleccionar.setMaximumSize(seleccionar.getMaximumSize());
+		Npanel.add(seleccionar);
 
-
+		//==================== ZOOM ++ ===================================
 		ZoomPlus = new JButton ("ZOOM ++");
 		ZoomPlus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -130,10 +146,10 @@ public class Interfaz {
 				finalImage = ZoomPanel.Zoom(originalImage, yScaleFactor, xScaleFactor );
 				mostrar(ImagFinalPanel, finalImage);
 			}
-		});
-		
+		});		
 		ZoomPlus.setMaximumSize(seleccionar.getMaximumSize());
-		
+		Npanel.add(ZoomPlus);
+		//==================== ZOOM -- =================================== 
 		ZoomMinus = new JButton ("ZOOM --");
 		ZoomMinus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -143,28 +159,38 @@ public class Interfaz {
 				  finalImage = ZoomPanel.Zoom(originalImage, yScaleFactor, xScaleFactor);
 				  mostrar(ImagFinalPanel, finalImage);
 			}
-		});
-		
+		});		
 		ZoomMinus.setMaximumSize(seleccionar.getMaximumSize());
-
-		girarIZQ = new JButton ("Girar IZQ");
-		
-		girarIZQ.setMaximumSize(seleccionar.getMaximumSize());
-
-		girarDCH = new JButton ("Girar DCH");
-		
-		girarDCH.setMaximumSize(seleccionar.getMaximumSize());
-
-		
-		recortar = new JButton ("Recortar");
-		
-		recortar.setMaximumSize(seleccionar.getMaximumSize());
-
-		Npanel.add(seleccionar);
 		Npanel.add(ZoomMinus);
-		Npanel.add(ZoomPlus);
-		Npanel.add(girarDCH);
+		//==================== GIRAR IZQ =================================== 
+		girarIZQ = new JButton ("Girar IZQ");
+		girarIZQ.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				 degree += 45;				 
+				 // RotatedPanel rot = new RotatedPanel();			
+				 finalImage = RotatedPanel.Rotar(originalImage, degree);
+				 ImagFinalPanel.removeAll();
+				 mostrar(ImagFinalPanel, finalImage);
+			}
+		});			
+		girarIZQ.setMaximumSize(seleccionar.getMaximumSize());
 		Npanel.add(girarIZQ);
+        // ==================== GIRAR DCH ===================================
+		girarDCH = new JButton ("Girar DCH");
+		girarDCH.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				 degree -= 45;				 
+				 // RotatedPanel rot = new RotatedPanel();			
+				 finalImage = RotatedPanel.Rotar(originalImage, degree);
+				 ImagFinalPanel.removeAll();
+				 mostrar(ImagFinalPanel, finalImage);
+			}
+		});		
+		girarDCH.setMaximumSize(seleccionar.getMaximumSize());
+		Npanel.add(girarDCH);
+		
+		recortar = new JButton ("Recortar");		
+		recortar.setMaximumSize(seleccionar.getMaximumSize());		
 		Npanel.add(recortar);
 
 		return Npanel;
